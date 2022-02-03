449ec95f3de6e01fa92a460ec5eb32a60a5e130b
Your Name <you@example.com>
2022-02-03 03:16:59 -0600

    Nay Castelan

---
+++ resources/views/htmls/index.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<!--
+	Stellar by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>MORAT :)</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="/css/main.css" />
+		<noscript><link rel="stylesheet" href="/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<span class="logo"><img src="C:\Users\NAYDELIN\Downloads\html5up-stellar\images\LOGO01.jpg" alt="" /></span>
+						<h1>MORAT</h1>
+						<p>Banda colombiana de folk-pop<br />
+					</header>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul>
+							<li><a href="#intro" class="active">¿QUIEN ES MORAT?</a></li>
+							<li><a href="#first">DISCOGRAFIA, MIEMBROS Y NOMINACIONES</a></li>
+							<li><a href="#second">¿PORQUE ME GUSTA?</a></li>
+
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Introduction -->
+							<section id="intro" class="main">
+								<div class="spotlight">
+									<div class="content">
+										<header class="major">
+											<h2>¿QUIEN ES MORAT?</h2>
+										</header>
+										<p>Formada originalmente en Bogotá en 2011. Se dieron a conocer en 2015 con su éxito Mi nuevo vicio. </p>
+										<P>Se dieron a conocer en 2015 con su éxito Mi nuevo vicio. ... Hasta finales de 2016 había formado parte de la banda Alejandro Posada, pero decidió abandonar el grupo, por lo que fue sustituido por Martín Vargas, hermano de Simón.</P>
+									</ul>
+									</div>
+									<span class="image"><img src="C:\Users\NAYDELIN\Downloads\html5up-stellar\images\02.png" alt="" /></span>
+								</div>
+							</section>
+
+						<!-- First Section -->
+							<section id="first" class="main special">
+								<header class="major">
+								</header>
+								<ul class="features">
+									<li>
+										<span class="icon solid major style1 fa-gem"></span>
+										<h3>MIEMBROS</h3>
+										<p>Sus integrantes son Juan Pablo Isaza Piñeros, Juan Pablo Villamil Cortés, Simón Vargas Morales y Martín Vargas Morales.</p>
+									</li>
+									<li>
+										<span class="icon major style3 fa-gem"></span>
+										<h3>DISCOGRAFIA</h3>
+										<p>Su discografia es un poco pequeña pero extensa, te daremos algunos nombres sus canciones</p>
+									</li>
+									<li>
+										<span class="icon major style5 fa-gem"></span>
+										<h3>ORIGEN DE SU NOMBRE</h3>
+										<p>Los integrantes de la banda solían ir a una finca en las afueras de Bogotá llamada "La Morat"...</p>
+									</li>
+								</ul>
+								<footer class="major">
+									<ul class="actions special">
+										<li><a href="{{router('generic')}}" class="button">Leer Más...</a></li>
+									</ul>
+								</footer>
+							</section>
+
+						<!-- Second Section -->
+							<section id="second" class="main special">
+								<header class="major">
+									<h1>¿PORQUE ESCOGI MORAT?</h1>
+								<p>Es una banda que en lo personal me gusta mucho por el tipo de musica que es, hay canciones con las que me siento identificada, y aparte porque me recuerada mi hermana.</p>
+								<span class="image"><img src="C:\Users\NAYDELIN\Downloads\html5up-stellar\images\04.jpg" alt="" /></span>
+								</header>
+							</section>
+							<ul class="icons">
+								<li><a href="https://www.facebook.com/Danny.castelan45" class="icon brands fa-facebook-f alt"><span class="label">Facebook</span></a></li>
+								<li><a href="https://www.instagram.com/danielita__cv/?hl=es-la" class="icon brands fa-instagram alt"><span class="label">Instagram</span></a></li>
+							</ul>
+						</section>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="/js/jquery.min.js"></script>
+			<script src="/js/jquery.scrollex.min.js"></script>
+			<script src="/js/jquery.scrolly.min.js"></script>
+			<script src="/js/browser.min.js"></script>
+			<script src="/js/breakpoints.min.js"></script>
+			<script src="/js/util.js"></script>
+			<script src="/js/main.js"></script>
+
+	</body>
+</html>
